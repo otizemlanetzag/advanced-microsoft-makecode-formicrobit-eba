b1d91735d359422943d4f726fc544e414faa9c65
otizemlanetzag <urielhetzroni@gmail.com>
2026-07-02 08:57:23 +0300

    Add Blockly variable block initialization

--- pxtblocks/plugins/newVariableField/index.ts
+++ pxtblocks/plugins/newVariableField/index.ts
@@ -1,1 +1,10 @@
 export * from "./fieldVariable";
+Blockly.Blocks['pxt_variable_name'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("שם המשתנה")
+            .appendField(new Blockly.FieldVariable("item"), "VAR");
+        this.setOutput(true, "String");
+        this.setColour(Blockly.Colors.variables);
+    }
+};
